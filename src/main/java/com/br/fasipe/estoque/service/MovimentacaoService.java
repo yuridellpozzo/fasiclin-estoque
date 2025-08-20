13e77c9f5d5ae8b7121e11da3a9c8fa44a43d7b1
matheusassuncaoo <matheus.assuncaomt18@gmail.com>
2025-08-20 15:33:37 -0400

    fix models

--- src/main/java/com/br/fasipe/estoque/service/MovimentacaoService.java
+++ src/main/java/com/br/fasipe/estoque/service/MovimentacaoService.java
@@ -7,6 +7,7 @@ import com.br.fasipe.estoque.repository.MovimentacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/br/fasipe/estoque/service/OrdemCompraService.java
+++ src/main/java/com/br/fasipe/estoque/service/OrdemCompraService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
